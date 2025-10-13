8f64e4e9e134680649579d3412043ae5b97a9e65
Phan Quoc Huy <36770627+harryphan18@users.noreply.github.com>
2025-10-13 16:03:59 +0700

    Update examUtils.js

--- src/app/utils/examUtils.js
+++ src/app/utils/examUtils.js
@@ -1,44 +1,70 @@
-// Hàm parse text thành JSON
-export function parseQuestions(text, limit) {
-    const blocks = text.split("/qb").map(b => b.trim()).filter(Boolean);
+// Hàm parse text thành JSON 
+export function parseQuestions(text, limit) { 
+   const blocks = text.split("/qb").map(b => b.trim()).filter(Boolean); 
 
-    const read = blocks.map((block, index) => {
-        const lines = block.split("\n").map(l => l.trim()).filter(Boolean);
-        const questionText = lines[0]; // dòng đầu tiên sau /qb là câu hỏi
-        const options = [];
-        let answer = null;
-        let answerOp = null;
+   const read = blocks.map((block, index) => { 
+       const lines = block.split("\n").map(l => l.trim()).filter(Boolean); 
+       let questionText = null;
+       let isQuestion = true;
 
-        for (let i = 1; i < lines.length; i++) {
-            const line = lines[i];
-            if (lines[i] === "/qe" || !line.startsWith("/o")) continue;
-            const text = line.replace("/o", "").replace("/a", "").trim();
-            const isAns = line.includes("/a");
-            if (isAns) {
-                answer = text;
-                answerOp = i;
-            }
-            options.push({ value: i, label: text, isCorrect: isAns });
-        }
-        return {
-            quesNum: index,
-            question: questionText,
-            options: shuffle(options),
-            answer,
-            answerOp
-        };
-    });
-    return shuffle(read).map((q, index) => ({
-        ...q,
-        quesNum: index
-    })).slice(0, limit);
-}
+       const options = []; 
+       const answerTxt = []; 
+       const answerVal = []; 
+       let opTextTemp = null;
+       let isAnsTemp = false;
+       let optionNum = 0;
 
-function shuffle(array) {
-    let arr = [...array]; // copy để tránh mutate
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
+       for (let i = 0; i < lines.length; i++) { 
+           const line = lines[i]; 
+           if (line.startsWith("/o")) {
+               isQuestion = false;
+           } else if (line.startsWith("/qe")) {
+               continue;
+           }
+
+           if (isQuestion) {
+               questionText = !questionText ? line : questionText + "\n" + line;
+           } else {
+               // text option
+               if (line.startsWith("/o")) {
+                   isAnsTemp = line.includes("/a");
+                   opTextTemp = line.replace("/o", "").replace("/a", "").trim();
+               } else {
+                   opTextTemp = opTextTemp + "\n" + line;
+               }
+               // finish text option
+               if (lines[i + 1].startsWith("/o") || lines[i + 1].startsWith("/qe")) {
+                   if (isAnsTemp) {
+                       answerTxt.push(opTextTemp);
+                       answerVal.push(optionNum);
+                   }
+                   options.push({ value: optionNum, label: opTextTemp, isCorrect: isAnsTemp });
+
+                   // reset text option
+                   optionNum++;
+               }
+           }
+       } 
+       return { 
+           quesNum: index, 
+           question: questionText.replace("/multi", "").trim(), 
+           options: shuffle(options), 
+           answerTxt: answerTxt, 
+           answerVal: answerVal,
+           isMulti: questionText.indexOf("/multi") > -1,
+       }; 
+   }); 
+   return shuffle(read).map((q, index) => ({ 
+       ...q, 
+       quesNum: index 
+   })).slice(0, limit); 
+} 
+
+function shuffle(array) { 
+  let arr = [...array]; // copy để tránh mutate 
+  for (let i = arr.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [arr[i], arr[j]] = [arr[j], arr[i]]; 
+  } 
+  return arr; 
 }
