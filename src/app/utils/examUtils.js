b56a3a0ffde19bf013ef05ec881fa49b0974cfab
Phan Quoc Huy <36770627+harryphan18@users.noreply.github.com>
2025-10-15 10:57:07 +0700

    Add files via upload

--- src/app/utils/examUtils.js
+++ src/app/utils/examUtils.js
@@ -1,70 +1,92 @@
 // Hàm parse text thành JSON 
 export function parseQuestions(text, limit) { 
-   const blocks = text.split("/qb").map(b => b.trim()).filter(Boolean); 
+    const blocks = text.split("/qb ").map(b => b.trim()).filter(Boolean); 
 
-   const read = blocks.map((block, index) => { 
-       const lines = block.split("\n").map(l => l.trim()).filter(Boolean); 
-       let questionText = null;
-       let isQuestion = true;
+    const read = blocks.map((block, index) => { 
+        const lines = block.split("\n").map(l => l.trim()).filter(Boolean); 
+        let questionText = null;
+        let isQuestion = true;
+        let isMulti = false;
 
-       const options = []; 
-       const answerTxt = []; 
-       const answerVal = []; 
-       let opTextTemp = null;
-       let isAnsTemp = false;
-       let optionNum = 0;
+        const options = []; 
+        const answerTxt = []; 
+        const answerVal = []; 
+        let opTextTemp = null;
+        let isAnsTemp = false;
+        let optionNum = 0;
 
-       for (let i = 0; i < lines.length; i++) { 
-           const line = lines[i]; 
-           if (line.startsWith("/o")) {
-               isQuestion = false;
-           } else if (line.startsWith("/qe")) {
-               continue;
-           }
+        for (let i = 0; i < lines.length; i++) { 
+            const line = lines[i]; 
+            if (line.startsWith("/o ")) {
+                isQuestion = false;
+            } else if (line.startsWith("/qe")) {
+                continue;
+            }
 
-           if (isQuestion) {
-               questionText = !questionText ? line : questionText + "\n" + line;
-           } else {
-               // text option
-               if (line.startsWith("/o")) {
-                   isAnsTemp = line.includes("/a");
-                   opTextTemp = line.replace("/o", "").replace("/a", "").trim();
-               } else {
-                   opTextTemp = opTextTemp + "\n" + line;
-               }
-               // finish text option
-               if (lines[i + 1].startsWith("/o") || lines[i + 1].startsWith("/qe")) {
-                   if (isAnsTemp) {
-                       answerTxt.push(opTextTemp);
-                       answerVal.push(optionNum);
-                   }
-                   options.push({ value: optionNum, label: opTextTemp, isCorrect: isAnsTemp });
+            if (isQuestion) {
+                if (!questionText) {
+                    questionText = line;
+                    isMulti = line ? line.indexOf("/multi ") > -1 : false;
+                } else {
+                    questionText = questionText + "\n" + line;
+                }
+            } else {
+                // text option
+                if (line.startsWith("/o ")) {
+                    isAnsTemp = line.includes("/a ");
+                    opTextTemp = line.replace("/o ", "").replace("/a ", "").trim();
+                } else {
+                    opTextTemp = opTextTemp + "\n" + line.trim();
+                }
+                // finish text option
+                if (lines[i + 1].startsWith("/o ") || lines[i + 1].startsWith("/qe")) {
+                    if (isAnsTemp) {
+                        answerTxt.push(opTextTemp);
+                        answerVal.push(optionNum);
+                    }
+                    options.push({ value: optionNum, label: opTextTemp, isCorrect: isAnsTemp });
 
-                   // reset text option
-                   optionNum++;
-               }
-           }
-       } 
-       return { 
-           quesNum: index, 
-           question: questionText.replace("/multi", "").trim(), 
-           options: shuffle(options), 
-           answerTxt: answerTxt, 
-           answerVal: answerVal,
-           isMulti: questionText.indexOf("/multi") > -1,
-       }; 
-   }); 
-   return shuffle(read).map((q, index) => ({ 
-       ...q, 
-       quesNum: index 
-   })).slice(0, limit); 
+                    // reset text option
+                    optionNum++;
+                }
+            }
+        } 
+        
+        return { 
+            quesNum: index, 
+            question: questionText ? questionText.replace("/multi ", "").trim() : questionText, 
+            options: shuffle(options), 
+            answerTxt: answerTxt, 
+            answerVal: answerVal,
+            isMulti: isMulti,
+        }; 
+    }); 
+    return shuffleQuestions(read).slice(0, limit); 
 } 
 
 function shuffle(array) { 
-  let arr = [...array]; // copy để tránh mutate 
-  for (let i = arr.length - 1; i > 0; i--) { 
-      const j = Math.floor(Math.random() * (i + 1)); 
-      [arr[i], arr[j]] = [arr[j], arr[i]]; 
-  } 
-  return arr; 
+    let arr = [...array]; // copy để tránh mutate 
+    for (let i = arr.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [arr[i], arr[j]] = [arr[j], arr[i]]; 
+    } 
+    return arr; 
 }
+
+export function shuffleQuestions(array, limit) {
+    var shuffled = shuffle(array).map((q, index) => ({ 
+        ...q, 
+        quesNum: index 
+    }));
+    if (limit) {
+        return shuffled.slice(0, limit);
+    } else {
+        return shuffled;
+    }
+}
+
+export const EXAM_SET = [
+    { value: 0, label: "Thi gây mê", file: "quiz_ta.txt" },
+    { value: 1, label: "Thi chuyên môn 2025", file: "quiz_chuyenmon2025.txt" },
+    { value: 2, label: "Thi chức danh 2024", file: "quiz_chucdanh2024.txt" },
+];
